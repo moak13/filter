ce9e02445c54fa4d70d4fd8db4e8b517d1ec9123
Ikenna Umeh <kennaumeh@gmail.com>
2023-09-25 12:03:34 +0100

    Fixed search function

--- lib/ui/bottom_sheets/notice/notice_sheet_model.dart
+++ lib/ui/bottom_sheets/notice/notice_sheet_model.dart
@@ -2,20 +2,18 @@ import 'package:filter/app/app.locator.dart';
 import 'package:filter/app/app.logger.dart';
 import 'package:filter/data_model/country_state.dart';
 import 'package:filter/services/json_resource_service.dart';
-import 'package:filter/ui/utils/string_util.dart';
 import 'package:stacked/stacked.dart';
 
 class NoticeSheetModel extends FutureViewModel<List<CountryState>?> {
   final _jsonResourceService = locator<JsonResourceService>();
   final _log = getLogger('NoticeSheetModel');
 
-  List<CountryState>? searchList;
-  List<CountryState>? memoryList;
+  List<CountryState> memoryList = [];
 
   Future<List<CountryState>?> _fetchStates() async {
     try {
       final states = await _jsonResourceService.loadStatesFromAsset();
-      memoryList = states;
+      memoryList = states!; // states cannot be null
       return states;
     } catch (e) {
       return null;
@@ -28,43 +26,20 @@ class NoticeSheetModel extends FutureViewModel<List<CountryState>?> {
   }
 
   void filter(String? value) {
+
+    if (value == null) return;
+
     _log.i('search value: $value');
 
-    if (memoryList == null && memoryList!.isEmpty) {
-      _log.i('no states in memory');
+    final List<CountryState> searchList = List.from(memoryList);
 
-      return;
+    if (value.isEmpty) {
+      data = searchList;
+      _log.i("List length: ${data!.length}");
+    } else {
+      data = searchList.where((item) => item.name!.toLowerCase().contains(value)).toList();
+      _log.i("List length: ${data!.length}");
     }
-
-    // Append into search list
-    searchList = memoryList;
-
-    /// Restore to showing all available states if user clears
-    /// the search box
-    if (StringUtil.isEmpty(value)) {
-      _log.i('search keyword is empty');
-      data?.clear();
-      data?.addAll(memoryList ?? []);
-      rebuildUi();
-      return;
-    }
-
-    _log.i('searchList Length: ${searchList?.length}');
-
-    /// filter in-memory available states for the search value
-    var result = searchList?.where((e) {
-      bool isAMatch =
-          e.name?.toLowerCase().contains(value!.trim().toLowerCase()) ?? false;
-      _log.i('isAMatch: $isAMatch');
-
-      return isAMatch;
-    }).toList();
-
-    if (result != null && result.isNotEmpty) {
-      data?.clear();
-      data = result;
-    }
-
     rebuildUi();
   }
 }
